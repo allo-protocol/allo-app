0b47e7e56dbd05da87cd94cd94281656b9b88915
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-01-10 11:49:46 +0100

    Default graphql endpoint to localhost indexer

--- extension/packages/app/lib/graphql.ts
+++ extension/packages/app/lib/graphql.ts
@@ -1,7 +1,7 @@
 import { Client, cacheExchange, fetchExchange } from "urql";
 
 const urls = {
-  [31337]: process.env.NEXT_PUBLIC_INDEXER_URL_31337,
+  [31337]: process.env.NEXT_PUBLIC_INDEXER_URL_31337 || "http://localhost:42069",
 };
 export function createClient(chainId: number) {
   const url = urls[chainId as keyof typeof urls];
