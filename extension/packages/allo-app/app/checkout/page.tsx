68696636a7a1c14cd15203e9e6dd5bf6ed48fb89
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-01-21 15:35:11 +0800

    Use SimpleGrants contract

--- extension/packages/allo-app/app/checkout/page.tsx
+++ extension/packages/allo-app/app/checkout/page.tsx
@@ -5,17 +5,14 @@ import { ClaimVoteTokens } from "~/components/claim-vote-tokens";
 import { AllocationForm } from "~/components/allocation/allocation-form";
 
 export default function CheckoutPage() {
-  const { AlloIRL, VoteToken } = useContracts();
+  const { SimpleGrants, ERC20Mock } = useContracts();
   return (
     <Page title="Checkout">
       <div className="mb-4">
-        <ClaimVoteTokens tokenAddress={VoteToken.address} />
+        <ClaimVoteTokens tokenAddress={ERC20Mock.address} />
       </div>
 
-      <AllocationForm
-        strategyAddress={AlloIRL.address}
-        tokenAddress={VoteToken.address}
-      />
+      <AllocationForm strategyAddress={SimpleGrants.address} tokenAddress={ERC20Mock.address} />
     </Page>
   );
 }
